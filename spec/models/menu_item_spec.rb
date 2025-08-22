f8caff94402dbdab7a6a698d7bfc147911aaf6c8
Rômulo Olveira <romuloffall@gmail.com>
2025-08-22 20:27:27 -0300

    #feature - add menu name and price  validations

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  end
 end
