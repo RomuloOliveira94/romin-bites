3e8469cb5cc9f45395e608553fc92711a74d0cb9
Rômulo Olveira <romuloffall@gmail.com>
2025-08-22 21:57:20 -0300

    #feature:  add uniqueness to menu item

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :menu do
     name { FFaker::Food.meat }
     description { FFaker::Lorem.paragraph }
+    restaurant
   end
 end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -6,7 +6,11 @@ RSpec.describe MenuItem, type: :model do
   end
 
   describe 'validations' do
+    let(:menu) { create(:menu) }
+    subject { build(:menu_item, menu: menu) }
+
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
     it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   end
 end
