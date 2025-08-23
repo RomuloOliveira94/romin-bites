c47148564847b6613f51571500ec9d6f40dfe8f9
Rômulo Olveira <romuloffall@gmail.com>
2025-08-22 21:51:52 -0300

    #feature - add menus relation to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_23_004515) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_23_005027) do
   create_table "menu_items", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -26,6 +26,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_23_004515) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_23_004515) do
   end
 
   add_foreign_key "menu_items", "menus"
+  add_foreign_key "menus", "restaurants"
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
+  describe 'associations' do
+    it { should have_many(:menus).dependent(:destroy) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
