19f5d712cff080163ac64469591ce7bae2c2e82c
Rômulo Olveira <romuloffall@gmail.com>
2025-08-24 00:19:29 -0300

    #refactor: clear db schema

--- db/schema.rb
+++ db/schema.rb
@@ -30,16 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_23_014126) do
     t.index ["menu_item_id"], name: "index_menu_items_menus_on_menu_item_id"
   end
 
-  create_table "menu_menu_items", force: :cascade do |t|
-    t.integer "menu_id", null: false
-    t.integer "menu_item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["menu_id", "menu_item_id"], name: "index_menu_menu_items_on_menu_id_and_menu_item_id", unique: true
-    t.index ["menu_id"], name: "index_menu_menu_items_on_menu_id"
-    t.index ["menu_item_id"], name: "index_menu_menu_items_on_menu_item_id"
-  end
-
   create_table "menus", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -58,7 +48,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_23_014126) do
 
   add_foreign_key "menu_items_menus", "menu_items"
   add_foreign_key "menu_items_menus", "menus"
-  add_foreign_key "menu_menu_items", "menu_items"
-  add_foreign_key "menu_menu_items", "menus"
   add_foreign_key "menus", "restaurants"
 end
